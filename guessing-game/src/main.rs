faccf8f8870e9ce589a2b7130e218ae8b498bb9f
XicoBM <franciscomarques232@gmail.com>
2025-06-18 00:51:02 +0100

    Complete all changes before final commit. Don't really remeber what was on the first commit, so I am not going to specify what was done.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -8,22 +8,33 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is {secret_number}");
+    //println!("The secret number is {secret_number}");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line!");
 
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a correct type of data.");
+                break;
+            }
+        }; 
 
-    println!("You guessed {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!"),
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
     }
 }
