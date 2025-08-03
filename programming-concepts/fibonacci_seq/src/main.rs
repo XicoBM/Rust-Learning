92b5cb0d01011a2bfa58bfefd47071581a1cacc6
XicoBM <franciscomarques232@gmail.com>
2025-08-03 20:16:20 +0100

    Do a small change on the function signature of the fibonacci function to return the whole sequence instead of just the last number. Also, tried to implement a 2nd function that used arrays but was not successful.

--- programming-concepts/fibonacci_seq/src/main.rs
+++ programming-concepts/fibonacci_seq/src/main.rs
@@ -6,24 +6,37 @@ fn main() {
         .read_line(&mut input)
         .expect("It was not possible to read the input. Please try again.");
 
-    let var: u32 = input
-        .trim()
-        .parse()
-        .expect("It was not possible to complete the process. Please try again.");
+    let var: u32 = input.trim().parse().expect("It was not possible to complete the process. Please try again.");
 
     fibonacci(var);
 }
 
 fn fibonacci(n: u32) -> () {
-    let res: u32;
+    let mut res: u32;
+    
+    for i in 0..=n {
+        if i <= 1 {
+            res = i;
+        } else {
+            res = (i-1) + (i-2);
+        }
+        println!("{}", res);
+    }
+} 
 
-    if n == 0 {
-        res = 0;
-    } else if n == 1 {
-        res = 1;
-    } else {
-        res = (n - 1) + (n - 2);
+/* 
+fn fibonacci_two(n: u32) -> () {
+    let c: u32 = n;
+    let res: [u32; c] = [0; c];
+
+    for i in 0..=n {
+        if i <= 1 {
+            res[i] = i;
+        } else {
+            res[i] = res[i-1] + res[i-2];
+        }
     }
 
-    println!("{}", res);
+    println!("{:?}", res);
 }
+*/
